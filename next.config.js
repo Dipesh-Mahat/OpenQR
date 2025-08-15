aa0f18587f5893bfaea0c4ba64a062dbc5ebc228
Dipesh-Mahat <dipesh.mahat.dev@gmail.com>
2025-08-15 09:38:46 +0545

    Add GitHub Actions workflow for deployment to GitHub Pages and update configuration files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Enable static exports
   images: {
     domains: ['localhost'],
     formats: ['image/webp', 'image/avif'],
+    unoptimized: true, // For static export
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
+  basePath: process.env.GITHUB_ACTIONS && process.env.NODE_ENV === 'production' ? '/OpenQR' : '',
+  trailingSlash: true, // Recommended for GitHub Pages
 }
 
 module.exports = nextConfig
